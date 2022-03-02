f26bd51a5d11c9f8cf89a42eb2242fcf4bc8878e
PudenceAhimbisibwe <prudeahimbisibwe@gmail.com>
2022-03-02 09:01:02 +0300

    first commit

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,5 @@
 fun main(){
+    // comparison
     val message = "come here"
     val message1 = "come here"
     if(message == message1){
@@ -29,6 +30,7 @@ fun main(){
     println(city.uppercase())
     println(city.lowercase())
     println(city.capitalize())
+    //empty and blank space
 
     val emptystring = ""
     val blankstring = " "
@@ -51,6 +53,26 @@ fun main(){
     val width = 100
     val state = "The width of the box is $width cm"
     println(state)
+
+    val name = "mitchelle Hope"
+    val r = name[4].toString()+name[5]+name[6]+name[7]+name[11]
+    println(r)
+    val z = name[4].toString()+name[5]+name[6]+name[7]
+    println(z)
+    val b = name[4].toString()+name[5]+name[6]+name[12]
+    println(b)
+
+    val sentence = "prudence comes from uganda. "+ "she is the most loved"
+    println(sentence.replace("uganda","kenya"))
+
+    val h = "Minage is a friend to susan. "+ "They sleep in the same room"
+    println(h.replace("a friend","an enemy"))
+
+    val note = "Lovelace is the most hated class at Akirachix. "+ "It has 33 students"
+    println(note.replace("hated","loved"))
+
+    val t = "Nevernce is the most loved at home."
+    println(t.replace("Nevernce","Prudence"))
 
 
 
